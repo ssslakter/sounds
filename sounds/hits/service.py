b41ba14e864cd156115306b1f954b1d07115cbd3
ssslakter <67190162+Ssslakter@users.noreply.github.com>
2024-05-28 01:36:08 +0700

    little fixes

--- sounds/hits/service.py
+++ sounds/hits/service.py
@@ -7,7 +7,7 @@ __all__ = ['DEF_FMT', 'split_audio', 'Config', 'apply_transforms', 'predict', 's
 import onnxruntime, requests, math, soundfile as sf
 from fastprogress import progress_bar
 from collections import deque
-import librosa, numpy as np, logging as l
+import librosa, numpy as np, logging as l, time
 from dataclasses import dataclass
 from pathlib import Path
 from fastcore.all import ifnone, call_parse
@@ -75,34 +75,47 @@ def stream_loop(stream_url, model, out_fldr='./preds', cfg = Config()):
     # init request, buffer and output folder
     out_fldr, tmp = Path(out_fldr), Path('temp.mp3')
     out_fldr.mkdir(exist_ok=True, parents=True)
-    r = requests.get(stream_url, stream=True)
     buff = deque(maxlen=cfg.buffer_size)
-    total_bytes = 0
+
     l.info("Starting stream loop")
-    try:
-        for block in r.iter_content(4096):
-            total_bytes += len(block)
-            buff.extend(block)
-            
-            # when buffer is filled with audio extract, write to file and predict
-            if len(buff) == cfg.buffer_size:
-                l.info("Buffer filled, extracting audio")
-                with open(tmp, 'wb') as f:
-                    f.write(bytes(buff))
-                    # clear buffer for next iteration
-                    for _ in range(cfg.buffer_stride): buff.popleft() 
-                l.info("Reading audio from {}")
-                audio, _ = librosa.load(tmp, sr=cfg.sr)
-                l.info(f"Read audio of length {len(audio)}. Predicting")
-                res = predict(audio, model, cfg)
-                for r in res:
-                    idx = len(list(out_fldr.iterdir()))
-                    fname = out_fldr/f'{idx}.wav'
-                    l.debug(f"Writing hit to {fname}")
-                    sf.write(fname, r, cfg.sr)
-    except KeyboardInterrupt:
-        l.info(f"Exiting stream loop. Total bytes read: {total_bytes}")
-        return
+    while True:
+        r = requests.get(stream_url, stream=True)
+        if r.status_code != 200:
+            # wait for 30 seconds before retrying
+            l.info(f"Got status code {r.status_code}. Retrying in 30 seconds")
+            time.sleep(30)
+            continue
+        l.info(f"Got status code {r.status_code}. Starting reading")  
+        total_bytes = 0
+        try:
+            for block in r.iter_content(4096):
+                total_bytes += len(block)
+                buff.extend(block)
+                
+                # when buffer is filled with audio extract, write to file and predict
+                if len(buff) == cfg.buffer_size:
+                    l.info("Buffer filled, extracting audio")
+                    with open(tmp, 'wb') as f:
+                        f.write(bytes(buff))
+                        # clear buffer for next iteration
+                        for _ in range(cfg.buffer_stride): buff.popleft() 
+                    l.info("Reading audio from {}")
+                    audio, _ = librosa.load(tmp, sr=cfg.sr)
+                    l.info(f"Read audio of length {len(audio)}. Predicting")
+                    res = predict(audio, model, cfg)
+                    for r in res:
+                        idx = len(list(out_fldr.iterdir()))
+                        fname = out_fldr/f'{idx}.wav'
+                        l.debug(f"Writing hit to {fname}")
+                        sf.write(fname, r, cfg.sr)
+        except requests.exceptions.StreamConsumedError:
+            l.info("Stream consumed, retrying")
+            continue
+        except KeyboardInterrupt: 
+            l.info("Got keyboard interrupt, exiting stream loop")
+            return
+        finally:
+            l.info(f"Total bytes read: {total_bytes}")
 
 # %% ../../nbs/hits_04_service.ipynb 5
 DEF_FMT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -122,8 +135,10 @@ def init_logger(name: str = None, level=l.INFO, format: str = None, handlers: li
 
 # %% ../../nbs/hits_04_service.ipynb 6
 @call_parse
-def main(url:str= 'http://localhost:8000/stream', model: str = "./model.onnx"):
+def main(url:str= 'http://localhost:8000/stream', model_path: str = "./model.onnx"):
     init_logger()
-    onnx_sess = onnxruntime.InferenceSession(model, providers=['CPUExecutionProvider'])
+    onnx_sess = onnxruntime.InferenceSession(model_path, providers=['CPUExecutionProvider'])
     model = lambda x: onnx_sess.run(None, {"input": x})
+    l.info(f"Starting stream loop with model {model_path}")
+    l.info(f"Streaming from {url}")
     stream_loop(url, model)
